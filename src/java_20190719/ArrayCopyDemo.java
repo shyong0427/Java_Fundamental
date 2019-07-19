8c7cfe047073fd9411441f6b0a46017379b80b0d
shyong0427 <shyong0427@daum.net>
2019-07-19 17:42:13 +0900

    2019-07-19 Update Ver.4

--- src/java_20190719/ArrayCopyDemo.java
+++ src/java_20190719/ArrayCopyDemo.java
@@ -6,7 +6,6 @@ public class ArrayCopyDemo {
 		String[] temp = new String[6];
 //// (원본배열, 원본배열의 복사시작위치, 복사 할 배열이름, 복사 할 배열의 시작위치, 복사 될 것의 목적위치)		
 ////  System.arraycopy(src, srcPos, dest, destPos, length);
-	////  System.arraycopy(src, srcPos, dest, destPos, length);////  System.arraycopy(src, srcPos, dest, destPos, length);
 		System.arraycopy(fruits, 0, temp, 2, 4);
 
 		temp[0] = "peach";
