7d471a905258a9f58430d4fe96e1ae9be3c5114a
shyong0427 <shyong0427@daum.net>
2019-07-25 16:58:40 +0900

    2019-07-25 Update Ver.3

--- src/java_20190725/step3/StopWatchDemo.java
+++ src/java_20190725/step3/StopWatchDemo.java
@@ -1,29 +1,14 @@
 package java_20190725.step3;
 
 public class StopWatchDemo {
-	public static void execute(StopMilliWatch sm) {
-		sm.start();
-		for (long i = 0; i < 30_000_000_000l; i++) { }
-		sm.stop();
-
-		double elapsedTime = sm.getElapsedTime();
-		System.out.printf("경과시간 : %.3f", elapsedTime);
-	}
-
-	public static void execute(StopNanoWatch sn) {
-		sn.start();
-		for (long i = 0; i < 30_000_000_000l; i++) { }
-		sn.stop();
-
-		double elapsedTime = sn.getElapsedTime();
-		System.out.printf("경과시간 : %.9f", elapsedTime);
-	}
-
 	public static void main(String[] args) {
 		StopNanoWatch s1 = new StopNanoWatch();
-		execute(s1);
+
+		s1.start();
+		for(long i=0;i<30_000_000_000l;i++) {	}
+		s1.stop();
 		
-		StopMilliWatch s2 = new StopMilliWatch();
-		execute(s2);
+		double elapsedTime = s1.getElapsedTime();
+		System.out.printf("경과시간 : %.9f", elapsedTime);
 	}
 }
--- src/java_20190725/step4/StopNanoWatch.java
+++ src/java_20190725/step4/StopNanoWatch.java
@@ -15,14 +15,3 @@ public class StopNanoWatch {
 		return (double)(stopTime - startTime) / (double) 1000000000;
 	}
 }
-
-//StopNanoWatch s1 = new StopNanoWatch();
-////currentTimeMillis()
-////1970. 1. 1 부터 지금까지 시간을 ms로 변환하라.
-//		s1.start();
-//		for (long i = 0; i < 30_000_000_000l; i++) { }
-//		s1.stop();
-//
-//		double elapsedTime = s1.getElapsedTime();
-//		System.out.printf("경과시간 : %.9f", elapsedTime);
-//	}
