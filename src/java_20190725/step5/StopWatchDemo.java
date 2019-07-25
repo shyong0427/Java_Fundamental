6f057c57c0c54c1e0c676f4c8cb1eabced7c8689
shyong0427 <shyong0427@daum.net>
2019-07-25 17:58:15 +0900

    2019-07-25 Update Ver.3

--- src/java_20190725/step5/StopWatchDemo.java
+++ src/java_20190725/step5/StopWatchDemo.java
@@ -6,11 +6,11 @@ public class StopWatchDemo {
 	}
 	
 	public static void main(String[] args) {
-//		StopNanoWatch s1 = new StopNanoWatch();
-//		execute(s1);
-//		
-//		StopMilliWatch s2 = new StopMilliWatch();
-//		execute(s2);
+		StopNanoWatch s1 = new StopNanoWatch();
+		execute(s1);
+		
+		StopMilliWatch s2 = new StopMilliWatch();
+		execute(s2);
 		
 		StopMicroWatch s3 = new StopMicroWatch();
 		execute(s3);
