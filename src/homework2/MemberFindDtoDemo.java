970f78d77c06bbcfaab4e4681fd5d78c352a3350
shyong0427 <shyong0427@daum.net>
2019-08-08 18:04:05 +0900

    2019-08-08 Update Ver.4

---
+++ src/homework2/MemberFindDtoDemo.java
@@ -0,0 +1,11 @@
+package homework2;
+
+public class MemberFindDtoDemo {
+	// 1. 객체를 생성한다.
+	MemberFindDto m1 = new MemberFindDto(1, "shyong0427@daum.net", "111", "2019-08-08", "2019-08-08");
+	// 2. 객체의 내용을 출력한다.
+	// 3. 객체의 내용을 수정한다.
+	// 4. 객체의 내용을 출력한다.
+	
+	MemgerFindDto m2 = new MemberFindDto(2, "shyong0427@daum.net", "2222", "2019-08-01", "2019-08-02");
+}
