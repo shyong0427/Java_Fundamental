e779ba3f367bc9ebc9d5098db73e64a446b44929
shyong0427 <shyong0427@daum.net>
2019-08-08 18:08:23 +0900

    2019-08-08 Update Ver.6

--- src/homework2/MemberFindDtoDemo.java
+++ src/homework2/MemberFindDtoDemo.java
@@ -10,9 +10,19 @@ public class MemberFindDtoDemo {
 		System.out.println(m1.getCode());
 		System.out.println(m1.getSdate());
 		System.out.println(m1.getEdate());
-		
 		// 3. 객체의 내용을 수정한다.
+		m1.setSeq(1111);
+		m1.setEmail("shyong0427_1@daum.net");
+		m1.setCode("12121212");
+		m1.setSdate("2019-09-01");
+		m1.setEdate("2019-09-02");		
 		// 4. 객체의 내용을 출력한다.
+		System.out.println(m1.getSeq());
+		System.out.println(m1.getEmail());
+		System.out.println(m1.getCode());
+		System.out.println(m1.getSdate());
+		System.out.println(m1.getEdate());
+		
 
 		MemgerFindDto m2 = new MemberFindDto(2, "shyong0427@daum.net", "2222", "2019-08-01", "2019-08-02");
 
